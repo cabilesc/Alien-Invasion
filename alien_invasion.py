a9abf9d8af6cc6009f03ecf939b0e1ef01ca5a88
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-11-12 07:58:04 -0800

    Created a function to update the bullets on the screen
    created a function _update_bullets to remove the bullets off the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,16 +29,9 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
-
-            # Get rid of bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-                len(self.bullets)
-
 
     def _check_events(self):
         # Respond to keypresses and mouse events
@@ -75,6 +68,17 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets"""
+        # Update bullet positions
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            len(self.bullets)
 
     def _update_screen(self):
         # Redraw the scree during during ach pass though the loop
