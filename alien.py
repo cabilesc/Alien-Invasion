e02df2b0445ede455df5b488a25db9f6550b11b3
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-11-12 08:20:30 -0800

    Added alien.py and alien bmp

---
+++ alien.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet"""
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+
+        # lead the alien image and set its rect attribute
+        self.image = pygame.image.load('/Users/christophercabiles/Desktop/repos/Alien-Invasion/images/alien11s (4).bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen 
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact hoizontal position
