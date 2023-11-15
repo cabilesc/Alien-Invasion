6f2b65ccaea3cb514d99ce6e5557323aef3218d6
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-11-15 15:58:34 -0800

    Ships Move left and right

--- settings.py
+++ settings.py
@@ -13,6 +13,12 @@ class Settings:
         # Ship Settings
         self.ship_speed = 3.5
 
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right: -1 represents left
+        self.fleet_direction = 1
+
         # bullet settings
         self.bullet_speed = 4.5
         self.bullet_width = 3
