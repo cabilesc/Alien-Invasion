e924e9224c99d7f76b710974732fd89ffcf4e280
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-11-15 18:36:24 -0800

    Add game stats and alien-ship collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,11 @@
 import sys
+from time import sleep
+
 import pygame
+
 from ship import Ship
 from settings import Settings
+from game_stats import GameStats
 from bullet import Bullet
 from alien import Alien
 
@@ -20,6 +24,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
         pygame.display.set_caption("Alien Invasion")
+
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -99,10 +106,30 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
 
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien"""
+        # Decrement ships_left
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaing bullets and aliens
+        self.bullets.empty()
+        self.aliens.empty()
+
+        # Create a new fleet and center the ship
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Pause
+        sleep(0.5)
+
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions"""
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print('Ship Hit!!!!')
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
