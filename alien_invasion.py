bf81fa9794f0e4e921b5a1a21dcb13f78c10b11d
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-11-15 19:06:28 -0800

    Game Over when Ships = 0

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,13 +35,19 @@ class AlienInvasion:
         # Set the background color
         # self.bg_color = (100, 100, 100)
 
+        # Start Alien Invasion in as an active state
+        self.game_active = True
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
             self.clock.tick(60)
 
@@ -108,19 +114,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
-        # Decrement ships_left
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Decrement ships_left
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaing bullets and aliens
-        self.bullets.empty()
-        self.aliens.empty()
+            # Get rid of any remaing bullets and aliens
+            self.bullets.empty()
+            self.aliens.empty()
 
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause
-        sleep(0.5)
+            # Pause
+            sleep(0.5)
+        else:
+            self.game_active = False
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions"""
