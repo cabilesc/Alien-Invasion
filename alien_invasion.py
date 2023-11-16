a88a7f532f23daff830f41e9111ceca9334f28c1
cabilesc <144515905+cabilesc@users.noreply.github.com>
2023-11-15 19:00:21 -0800

    Resets when aliens that reach the bottom of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -130,6 +130,18 @@ class AlienInvasion:
         # Look for alien-ship collisions
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+            
+
+        # Look for aliens hitting the bottom of the screen
+        self._check_aliens_bottom()
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen"""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # Treat this the same as if the ship got hit
+                self._ship_hit()
+                break
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
